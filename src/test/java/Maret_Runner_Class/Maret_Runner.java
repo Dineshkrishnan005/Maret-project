3203db2b628fe18b6d0f7a1a471d027825405953
Dineshkrishnan005 <dinesh.krishnan@roundrockabaschool.onmicrosoft.com>
2023-04-04 13:13:54 +0530

    Maret Updated...

---
+++ src/test/java/Maret_Runner_Class/Maret_Runner.java
@@ -0,0 +1,23 @@
+package Maret_Runner_Class;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+
+  @CucumberOptions(features = "C:\\Users\\Dinesh K\\eclipse-workspace\\Maret\\src\\test\\resources\\Feature\\Maret_Signup.feature",
+  glue= "Maret_Step_Definition" , 
+  plugin={"pretty","json:Report/NewMaretSignUp_Test1.json"
+		  ,"html:CucumberReport/NewMaretSignUp_Test1"
+		  ,"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+  publish = true, 
+  monochrome= true,
+  dryRun= false)
+ 
+
+public class Maret_Runner {
+
+	 
+}
