e0589c7a14e0fe6b6a146fa0b48872151b661ce0
Dineshkrishnan005 <dinesh.krishnan@roundrockabaschool.onmicrosoft.com>
2023-04-04 18:51:13 +0530

    Recent youtube commit...

--- src/test/java/Maret_Runner_Class/Maret_Runner.java
+++ src/test/java/Maret_Runner_Class/Maret_Runner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 
-  @CucumberOptions(features = "C:\\Users\\Dinesh K\\eclipse-workspace\\Maret\\src\\test\\resources\\Feature\\Youtube.feature",
+  @CucumberOptions(features = "C:\\Users\\Dinesh K\\eclipse-workspace\\Maret\\src\\test\\resources\\Feature",
   glue= "Maret_Step_Definition" , 
   plugin={"pretty","json:Report/NewMaretSignUp_Test.json"
 		  ,"html:CucumberReport/NewMaretSignUp_Test1"
