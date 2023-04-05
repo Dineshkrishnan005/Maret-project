b5924d3d3ee088f044f241a107d93a25717ce1a7
Dineshkrishnan005 <dinesh.krishnan@roundrockabaschool.onmicrosoft.com>
2023-04-05 11:49:34 +0530

    Final Mynthra commit..

--- src/test/java/Maret_Runner_Class/Maret_Runner.java
+++ src/test/java/Maret_Runner_Class/Maret_Runner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 
-  @CucumberOptions(features = "C:\\\\Users\\\\Dinesh K\\\\eclipse-workspace\\\\Maret\\\\src\\\\test\\\\resources\\\\Feature",
+  @CucumberOptions(features = "C:\\Users\\Dinesh K\\eclipse-workspace\\Maret\\src\\test\\resources\\Feature",
   glue= "Maret_Step_Definition" , 
   plugin={"pretty","json:Report/NewMaretSignUp_Test1.json"
 		  ,"html:CucumberReport/NewMaretSignUp_Test2"
